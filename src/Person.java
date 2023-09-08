28a49190a999ddc568296f5a5979ca32b1976bbe
Carsten-Christian Platz <platz@citeq.de>
2023-09-08 08:50:45 +0200

    setup person class

---
+++ src/Person.java
@@ -0,0 +1,43 @@
+public class Person {
+    String name, firstName;
+    int height, weight;
+
+    public Person(String name, String firstName, int height, int weight) {
+        this.name = name;
+        this.firstName = firstName;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
