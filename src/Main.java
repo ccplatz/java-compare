f055a57ff6a1a38a4cb62c7933281dc9d8b61759
Carsten-Christian Platz <platz@citeq.de>
2023-09-08 09:57:21 +0200

    implement person class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person person1 = new Person();
+        System.out.println(person1.toString());
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,41 @@
+import java.text.DecimalFormat;
+import java.util.*;
+
 public class Person {
     String name, firstName;
-    int height, weight;
+    double height, weight;
 
-    public Person(String name, String firstName, int height, int weight) {
-        this.name = name;
-        this.firstName = firstName;
-        this.height = height;
-        this.weight = weight;
+    ArrayList<String> firstNames = new ArrayList<>(Arrays.asList("Abraham", "Berta", "Carsten", "Detlef", "Edeltraud", "Ferdinand"));
+    ArrayList<String> names = new ArrayList<>(Arrays.asList("Asbach", "Braun", "Conrad", "Dreher", "Engel", "Fischer"));
+
+    public Person() {
+        Collections.shuffle(names);
+        this.name = names.get(0);
+        Random random = new Random();
+        char middleInitial = (char) (random.nextInt(26) + 'a');
+        Collections.shuffle(firstNames);
+        this.firstName = firstNames.get(0) + " " + Character.toUpperCase(middleInitial) + ".";
+        HashMap<String, Double> weightAndHeight = getHeightAndWeight();
+        this.height = weightAndHeight.get("height");
+        this.weight = weightAndHeight.get("weight");
+    }
+
+    private static HashMap<String, Double> getHeightAndWeight() {
+        HashMap<String, Double> weightAndHeight = new HashMap<>();
+
+        Random random = new Random();
+        double bmi = (double) random.nextInt(1600, 4600) / 100;
+        double height = (double) random.nextInt(140, 206) / 100;
+        double weight = round(bmi * Math.pow(height, 2));
+
+        weightAndHeight.put("height", height);
+        weightAndHeight.put("weight", weight);
+
+        return weightAndHeight;
+    }
+
+    private static double round(double d) {
+        return Math.round(d * 100.0) / 100.0;
     }
 
     public String getName() {
@@ -25,19 +54,34 @@ public class Person {
         this.firstName = firstName;
     }
 
-    public int getHeight() {
+    public double getHeight() {
         return height;
     }
 
-    public void setHeight(int height) {
+    public void setHeight(double height) {
         this.height = height;
     }
 
-    public int getWeight() {
+    public double getWeight() {
         return weight;
     }
 
-    public void setWeight(int weight) {
+    public void setWeight(double weight) {
         this.weight = weight;
+    }
+
+    public double getBmi() {
+        return round(weight / Math.pow((double) height, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Name: '" + getName() + '\'' +
+                ", Vorname: '" + getFirstName() + '\'' +
+                ", Größe: " + getHeight() + " m" +
+                ", Gewicht: " + getWeight() + " kg" +
+                ", BMI: " + getBmi() +
+                '}';
     }
 }
